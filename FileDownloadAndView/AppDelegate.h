b55197cbb405982fc1ed69b0b2fa23021777be34
Barry <barry@Barrys-MacBook-Pro.local>
2014-01-02 10:34:09 -0500

    Initial Commit

---
+++ FileDownloadAndView/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FileDownloadAndView
+//
+//  Created by Barry on 1/2/14.
+//  Copyright (c) 2014 BICSI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ FileDownloadAndView/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  FileDownloadAndView
+//
+//  Created by Barry on 1/2/14.
+//  Copyright (c) 2014 BICSI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
